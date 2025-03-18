923be0e69c78ac6468f6df53b3763af891dcadd4
Anirudha Hensh <154799988+aniraja@users.noreply.github.com>
2025-03-18 16:11:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ const App = () => {
 
   const handlePredict = async () => {
     try {
-      const response = await axios.post('http://127.0.0.1:5000/predict', {
+      const response = await axios.post('https://stock-prediction-api.onrender.com/predict', {
+
         ticker,
         days: parseInt(days),
       });
